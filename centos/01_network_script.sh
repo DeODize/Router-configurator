462f95cda6d0d8f8632f706f42b846222b56e63d
Dimok <kuznetzoffdm1t@yandex.ru>
2026-08-05 12:06:36 +0700

    update network script

--- centos/01_network_script.sh
+++ centos/01_network_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-nmcli con add type ethernet ifname ens33 con-name NAT ipv4.addresses 192.168.0.40/24 ipv4.gateway 192.168.0.1 ipv4.method manual
+
+#nmcli con add type ethernet ifname ens33 con-name NAT ipv4.addresses 192.168.0.40/24 ipv4.gateway 192.168.0.1 ipv4.method manual
 nmcli con add type ethernet ifname ens36 con-name LAN ipv4.addresses 172.17.0.1/24 ipv4.method manual ipv4.routes "192.168.56.0/24 172.17.0.3"
